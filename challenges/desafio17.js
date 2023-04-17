1f3e300083759cd68b98c9b9681f8e2898eee4f6
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 18:20:19 -0300

    req17 - concluido

---
+++ challenges/desafio17.js
@@ -0,0 +1,5 @@
+db.resumoProdutos.insert(
+  { franquia: "McDonalds",
+    totalProdutos: db.produtos.countDocuments({}) });
+
+db.resumoProdutos.find({}, { _id: false, franquia: true, totalProdutos: true });
