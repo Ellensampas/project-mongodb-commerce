c3afb2b52f4b9fc35c44e04f53eb694301644fba
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 18:24:20 -0300

    req17 - corrigindo erros

--- challenges/desafio17.js
+++ challenges/desafio17.js
@@ -1,5 +1,6 @@
 db.resumoProdutos.insert(
   { franquia: "McDonalds",
-    totalProdutos: db.produtos.countDocuments({}) });
+    totalProdutos: db.produtos.countDocuments({}) },
+);
 
 db.resumoProdutos.find({}, { _id: false, franquia: true, totalProdutos: true });
