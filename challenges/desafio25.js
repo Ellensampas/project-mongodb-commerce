7255cd6c83dc94a244a7ed353030c8f81f7663c0
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 20:19:49 -0300

    req25 - concluido

---
+++ challenges/desafio25.js
@@ -0,0 +1,6 @@
+db.produtos.updateMany(
+  { 
+    valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gte: 40} } } },
+  { $addToSet: { tags: "muito sódio" } });
+
+db.produtos.find({}, {nome: true, _id: false, tags: true});
