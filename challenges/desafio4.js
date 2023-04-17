5316b7c3ca1c58e5bb7d39440f52ab704dbc6f3b
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 15:57:47 -0300

    req04 - corrigindo erros, req05 - concluido

--- challenges/desafio4.js
+++ challenges/desafio4.js
@@ -1,1 +1,4 @@
-db.produtos.find({ vendidos: { $gt: 50, $lt: 100 } }, { nome: true, vendidos: true, _id: false }).sort({ vendidos: 1 });
+db.produtos.find(
+  { vendidos: { $gt: 50, $lt: 100 } }, 
+  { nome: true, vendidos: true, _id: false },
+  ).sort({ vendidos: 1 });
---
+++ challenges/desafio5.js
@@ -0,0 +1,14 @@
+db.produtos.find(
+  {
+    $or: [
+      { curtidas: 36 },
+      { vendidos: 85 },
+    ],
+  },
+  { 
+    nome: true,
+    curtidas: true,
+    vendidos: true,
+    _id: false,
+  } 
+  );
