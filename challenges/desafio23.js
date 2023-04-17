b745b06472a6e88716ed9ab4dc508e22057bb62c
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 20:01:28 -0300

    req23 - corrigindo erros

--- challenges/desafio23.js
+++ challenges/desafio23.js
@@ -1,5 +1,4 @@
 db.produtos.updateMany({},
-  { $push: { tags: { $each : ["combo", "tasty"], $sort: 1 } } },
-);
+  { $push: { tags: { $each: ["combo", "tasty"], $sort: 1 } } });
 
 db.produtos.find({}, { nome: true, tags: true, _id: false });
