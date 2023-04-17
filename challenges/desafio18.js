8b16a6772c9c97b8abf581d370489eeb98994160
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 18:34:56 -0300

    req18 - corrigindo erros

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,8 +1,8 @@
 db.produtos.updateMany(
   {
-    nome: { $all: ["Big Mac", "Quarteirão com Queijo"] },
+    nome: { $in: ["Big Mac", "Quarteirão com Queijo"] },
   },
-  { $addToSet: { ingredientes: "bacon" } },
+  { $addToSet: { ingredientes: "bacon" } }
 );
 
 db.produtos.find({}, { _id: false, nome: true, ingredientes: true });
