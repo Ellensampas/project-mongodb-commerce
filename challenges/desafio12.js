23123bf6adf616522f65bc3fb727498d3a201ebd
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 17:16:14 -0300

    req12 - corrigindo erros

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -2,4 +2,4 @@ db.produtos.updateMany(
   { nome: { $ne: "McChicken" } },
   { $addToSet: { ingredientes: "ketchup" } },
 );
-db.produtos.find( {}, { _id: false, nome: true, ingredientes: true });
+db.produtos.find({}, { _id: false, nome: true, ingredientes: true });
