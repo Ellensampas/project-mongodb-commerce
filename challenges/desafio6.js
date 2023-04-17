a9be48f9236357f15f161cd1e11ab90169a38c50
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 16:07:11 -0300

    req06 - concluido

---
+++ challenges/desafio6.js
@@ -0,0 +1,13 @@
+db.produtos.find(
+  {
+    $and: [
+      { curtidas: { $gt: 10 } },
+      { curtidas: { $lt: 100 } },
+    ],
+  }, 
+  { 
+    nome: true,
+    curtidas: true,
+    _id: false,
+  }, 
+  );
