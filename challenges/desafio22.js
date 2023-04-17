c942547731447c26d21a8633e23edf2bdecb62cc
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 19:44:37 -0300

    req22 - concluido

---
+++ challenges/desafio22.js
@@ -0,0 +1,18 @@
+db.produtos.updateMany({},
+  { $set: { vendasPorDia: [0, 0, 0, 0, 0, 0, 0] } });
+
+db.produtos.updateOne({ nome: "Big Mac" },
+  { $inc: { "vendasPorDia.3": 60 } });
+
+db.produtos.updateOne(
+  {
+     tags: { $elemMatch: { $all: ["bovino"] } },
+  },
+  { $inc: { "vendasPorDia.6": 120 } });
+
+db.produtos.find({}, 
+  { 
+    _id: false, 
+    nome: true, 
+    vendasPorDia: true
+  });
