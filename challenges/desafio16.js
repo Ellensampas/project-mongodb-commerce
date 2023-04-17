72606437d02252be90ce4a673d473d793cffdc29
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 18:03:53 -0300

    req15 - corrigindo erros, req16 - concluido

--- challenges/desafio16.js
+++ challenges/desafio16.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({ nome: "Big Mac" },
-  { $currentDate: { ultimaModificacao: { $type: "date"} } });
+  { $currentDate: { ultimaModificacao: { $type: "date" } } });
 db.produtos.find({}, { _id: false, nome: true });
