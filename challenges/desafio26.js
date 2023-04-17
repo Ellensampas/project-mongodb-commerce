4f817bdbbd4571698c9ff0c6ab4fdb2973fac6f3
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 20:24:50 -0300

    req26 - corrigindo erros

--- challenges/desafio26.js
+++ challenges/desafio26.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany(
   { 
     valoresNutricionais: { $elemMatch: { tipo: "sódio", percentual: { $gt: 20, $lt: 40 } } } },
-  { $addToSet: { tags: "contém sódio" } }
+  { $addToSet: { tags: "contém sódio" } },
 );
 
 db.produtos.find({}, { nome: true, _id: false, tags: true });
