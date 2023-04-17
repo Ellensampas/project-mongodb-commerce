ee9901ae3ae39f26ad838e78cfb51ffe8108af80
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 18:02:41 -0300

    req15 - corrigindo erros, req16 - concluido

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,7 +1,7 @@
 db.produtos.updateMany({},
   { $set: { avaliacao: 0 } });
-db.produtos.updateMany({ tags: { $all: ["bovino"] } },
+db.produtos.updateMany({ tags: { $in: ["bovino"] } },
   { $inc: { avaliacao: 5 } });
-db.produtos.updateMany({ tags: { $all: ["aves"] } },
+db.produtos.updateMany({ tags: { $in: ["aves"] } },
   { $inc: { avaliacao: 3 } });
 db.produtos.find({}, { _id: false, nome: true, avaliacao: true });
