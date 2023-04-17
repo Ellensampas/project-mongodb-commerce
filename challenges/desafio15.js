8498c2d6663b24a0690bc47f8d7cfd55920f09bd
ellen Sampaio <ellenssbarross@gmail.com>
2023-04-17 18:12:31 -0300

    req15 - corrigindo erro

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,7 +1,17 @@
-db.produtos.updateMany({},
-  { $set: { avaliacao: 0 } });
-db.produtos.updateMany({ tags: { $elemMatch: { $in: ["bovino"] } } },
-  { $inc: { avaliacao: 5 } });
-db.produtos.updateMany({ tags: { $elemMatch: { $in: ["aves"] } } },
-  { $inc: { avaliacao: 3 } });
+db.produtos.updateMany({}, { $set: { avaliacao: 0 } });
+
+db.produtos.updateMany(
+  { 
+    tags: { $elemMatch: { $in: ["bovino"] } },
+  },
+  { $inc: { avaliacao: 5 } },
+);
+
+db.produtos.updateMany(
+  { 
+    tags: { $elemMatch: { $in: ["ave"] } }, 
+  },
+  { $inc: { avaliacao: 3 } },
+);
+
 db.produtos.find({}, { _id: false, nome: true, avaliacao: true });
